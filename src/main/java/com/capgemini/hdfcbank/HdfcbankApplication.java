f9dde9f927c7b1003f7a73f4b0c5be69ad26699c
shdrao <shdrao@IN-MUM-LDVM038.corp.capgemini.com>
2018-09-26 06:32:02 -0400

    project created

---
+++ src/main/java/com/capgemini/hdfcbank/HdfcbankApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.hdfcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfcbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfcbankApplication.class, args);
+	}
+}
