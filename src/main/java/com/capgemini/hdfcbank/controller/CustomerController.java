0b11bb3faffb176fe99f92f3d508ad0dcf75bbc8
shdrao <shdrao@IN-MUM-LDVM038.corp.capgemini.com>
2018-09-27 01:55:26 -0400

    af

--- src/main/java/com/capgemini/hdfcbank/controller/CustomerController.java
+++ src/main/java/com/capgemini/hdfcbank/controller/CustomerController.java
@@ -1,21 +1,29 @@
 package com.capgemini.hdfcbank.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.capgemini.hdfcbank.entities.Customer;
 import com.capgemini.hdfcbank.service.CustomerService;
 
 @Controller
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
-	@RequestMapping("/g")
-	public String checking() {
-//		customerService.addCustomer(new Customer("A", 3, "a@a.in", "xyz", "123", LocalDate.now()
-//				, new BankAccount("Savings",10,9999)));
+
+	@RequestMapping("/")
+	public String checking(Model model, HttpServletRequest request) {
+		Customer customer = new Customer(null, Long.parseLong(request.getParameter("custId")), null, null,
+				request.getParameter("password"), null, null);
+
+		customerService.authenticateCustomer(customer);
+		if (customer.getEmailId() != null)
+			return "index";
 		return "index";
 	}
 
